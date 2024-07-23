e0a3464720ed7e2a19913215e8562325fe9dcde1
Mark McCluskey <mark.d.mccluskey@gmail.com>
2024-07-22 19:58:30 -0500

    attempted fix

--- liesel_app.py
+++ liesel_app.py
@@ -50,12 +50,12 @@ df_stops['ttrip']=df_stops['trip'].cumsum()
 # -----------------------
 
 st.set_page_config(page_title=page_title,layout=layout)
-st.title("Data Analytics on Real World Data: Gas Receipts")
+st.header("Data Analytics on Real World Data: Gas Receipts")
 
 tab1, tab2, tab3 = st.tabs(["Background", "Enter Liesel", "Full Circle"])
 
 with tab1:
-    st.header("Background")
+    st.subheader("Background")
     st.markdown("""
                 When I was younger, I had a co-worker that kept a log of every time he put gas in his truck. He logged the mileage, the gas consumption, dates, everything. At the time, it seemed weird. Why go to the trouble? What could you possibly do with all of that information? _How could that possibly be worth the effort?_
 
@@ -64,7 +64,7 @@ with tab1:
                 I went to college and earned a Bachelor of Science and Master of Science, both in Mathematics. For the past 25 years, I have been doing data science in one form or another. It wasn't even called _\"data science\"_ back when I started.
                 """)
 with tab2:
-    st.header("Enter Liesel")
+    st.subheader("Enter Liesel")
     st.markdown("""
                 I bought my car, a Grey 2019 Volkswagen Golf, in June 2019. My prior car was also a Volkswagen, a 2001 Blue Lagoon Jetta 1.8T that I named _Lorelei_ after a Siren in German mythology. I'm a sentimental person, and I name inanimate objects with which I spent a lot of time, so obviously I was naming the Golf, too. Just like with boats, I feel cars have feminine spirits. I also name things based on their origin. _Lorelei_ was a German car and needed a German name. I had a Ducati Monster 695 Dark named _Minerva_ after the Roman goddess. So the Golf needed a feminine German name.
                 
@@ -73,7 +73,7 @@ with tab2:
                 _Liesel_ was purchased to be a second car for our household. I didn't need to use it that often to commute to my job, maybe once or twice a week. We have offices out in the suburbs and downtown Chicago, both not far from the train that runs near my house. Then the pandemic struck. I had been using it infrequently before; at that point, I wasn't using it at all. It took a long time for things to get back to anything resembling normalcy.
                 """)            
 with tab3:
-    st.header("Full Circle")
+    st.subheader("Full Circle")
     st.markdown("""
                 When I drove Liesel home, I remembered my co-worker and his logging of gas receipts. My view of the whole process had completely turned around. Gas receipts are reflections of a life in motion. Where have you been, and where are you now? Are you moving more or less than last week, last month, or last year? Do you move less when it costs more to do so?
 
